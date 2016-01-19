17e5f1c40b6d02d178b637706d67f53702c84cf6
m516 <m516@m516>
2016-01-19 14:34:07 +0800

    將functionalTests.py改為一個app，使用LiveserverTestCase

--- superLists/lists/tests.py
+++ superLists/lists/tests.py
@@ -1,39 +1,74 @@
 from django.core.urlresolvers import resolve
 from django.http.request import HttpRequest
-from django.shortcuts import render
 from django.test import TestCase
 
 from lists.views import homePage
-
+from lists.models import Item
 
 class HomePageTest(TestCase):
+    
     
     def test_root_url_to_homePage_view(self):
         found = resolve('/')
         self.assertEqual(found.func, homePage)
-
-    '''
-    def test_homePage_returns_correct_HTML(self):
-        request = HttpRequest()
-        response = homePage(request)
-        if response:
-            response = response.content.decode('UTF-8')
-        expectedHTML = render(request, 'lists/home.html')
-        if expectedHTML:
-            expectedHTML = expectedHTML.content.decode('UTF-8')
-        self.assertEqual(response, expectedHTML)
-    '''    
+        
+        
     def test_homePage_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['itemText'] = '新的項目'
         
         response = homePage(request)
+        
+        self.assertEqual(Item.objects.all().count(), 1)
+        newItem = Item.objects.first()
+        self.assertEqual(newItem.text, '新的項目')
+        
+    
+    def text_homePage_redirect_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['itemText'] = '新的項目'
+        
+        response = homePage(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    
+    def test_homePage_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        homePage(request)
+        self.assertEqual(Item.objects.count(), 0)
+    
+    
+    def test_homePage_display_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        
+        request = HttpRequest()
+        response = homePage(request)
         if response:
             response = response.content.decode('UTF-8')
-        self.assertIn('新的項目', response)
         
-        expectedHTML = render(request, 'lists/home.html', {'itemText':'新的項目'})
-        if expectedHTML:
-            expectedHTML = expectedHTML.content.decode('UTF-8')
-        self.assertEqual(response, expectedHTML)
+        self.assertIn('itemey 1', response)
+        self.assertIn('itemey 2', response)    
+    
+class ItemModelTest(TestCase):
+    
+    def test_save_and_retrieving_items(self):
+        firstItem = Item()
+        firstItem.text = '第一個清單項目'
+        firstItem.save()
+        
+        secondItem = Item()
+        secondItem.text = '第二個清單項目'
+        secondItem.save()
+
+        savedItems = Item.objects.all()
+        self.assertEqual(savedItems.count(), 2)
+        
+        firstSavedItem = savedItems[0]
+        secondSavedItem = savedItems[1]
+        self.assertEqual(firstSavedItem.text, '第一個清單項目')
+        self.assertEqual(secondSavedItem.text, '第二個清單項目')
+        
