5e629e0d73b30aaa8e13dba475dce28c179400e1
frankchin <frank65501830@gmail.com>
2016-02-05 18:05:45 +0800

    Update wsgi.py

--- superLists/superLists/wsgi.py
+++ superLists/superLists/wsgi.py
@@ -13,4 +13,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superLists.settings")
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+
+if 'DYNO' in os.environ:     # For Heroku
+    from dj_static import Cling  
+    application = Cling(get_wsgi_application())
+else:
+    application = get_wsgi_application()
+
