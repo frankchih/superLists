23ce3213cbbc9ff57cfaba2869aaf618777d0c52
frankchin <frank65501830@gmail.com>
2016-02-16 13:48:59 +0800

    static bug fix

--- superLists/superLists/settings.py
+++ superLists/superLists/settings.py
@@ -132,5 +132,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG==False:     # Running on Heroku
-    STATIC_ROOT = 'staticfiles'     # Static asset configuration
+# Running on Heroku
+STATIC_ROOT = 'staticfiles'     # Static asset configuration
